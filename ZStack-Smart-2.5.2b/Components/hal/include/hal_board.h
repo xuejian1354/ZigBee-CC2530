3cd6ee6a9983b872c21d4097bcc7d578c8c03b1d
Sam Chen <xuejian1354@163.com>
2015-06-09 13:43:41 +0800

    copy branch for single product Air Controller

--- ZStack-Smart-2.5.2b/Components/hal/include/hal_board.h
+++ ZStack-Smart-2.5.2b/Components/hal/include/hal_board.h
@@ -4,19 +4,5 @@
 #define HAL_UART_DMA_DISABLE_RTCT	/*串口RT , CT 引脚无效*/
 #define HOLD_INIT_AUTHENTICATION	/* 自定义初始化认证方式 */
 
-#include "Connector_Board_cfg.h"
-#include "DemoBase_Board_cfg.h"
-
-#include "LightSwitchOne_Board_cfg.h"
-#include "LightSwitchTwo_Board_cfg.h"
-#include "LightSwitchThree_Board_cfg.h"
-#include "LightSwitchFour_Board_cfg.h"
-#include "Alarm_Board_cfg.h"
-#include "IRDetection_Board_cfg.h"
-#include "DoorSensor_Board_cfg.h"
-#include "DustSensor_Board_cfg.h"
-#include "IRRelay_Board_cfg.h"
-
-
-
+#include "AirController_Board_cfg.h"
 #include "hal_board_cfg.h"
